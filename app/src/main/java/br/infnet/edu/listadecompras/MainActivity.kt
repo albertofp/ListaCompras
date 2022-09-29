6d9c5543b6fa7a668dc856e5e32fd442b9f042b0
Alberto Pluecker <albertopluecker@gmail.com>
2022-09-29 21:11:06 +0200

    - Set onclicklistener to the button that creates a new Item - started working on ViewModel/LiveData implementation

---
+++ app/src/main/java/br/infnet/edu/listadecompras/MainActivity.kt
@@ -0,0 +1,21 @@
+package br.infnet.edu.listadecompras
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import br.infnet.edu.listadecompras.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val btnIniciar = findViewById<View>(R.id.btnIniciar)
+        btnIniciar.setOnClickListener {
+            val homeIntent = Intent(this, activityHome::class.java)
+            startActivity(homeIntent)
+        }
+    }
+}
